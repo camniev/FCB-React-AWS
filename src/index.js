57fde543919b1a33c55439236ec0cf9c45cc1ea4
Cameron Nieve <camnieve@gmail.com>
2022-01-20 16:30:54 +0800

    Added some CSS styles and implemented inline styling

--- src/index.js
+++ src/index.js
@@ -8,11 +8,24 @@ import "./index.css";
 // }
 
 function App() {
+
+  //inline css but will require an object
+  const paraStyle = {
+    "marginBottom": "15px"
+  };
+
   return(
     <div className="app">
       <header className="app-header">
         <h1>My Amazing To-do List App</h1>
-        <p>The most simple and amazing to-do list React app.</p>
+
+        {/* with inline css (jsx style) */}
+        <p style={paraStyle}>The most simple and amazing to-do list React app.</p>
+        <ul>
+          <li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
+          <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook</li>
+          <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook</li>
+        </ul>
       </header>
     </div>
   );
