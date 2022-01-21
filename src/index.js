d7cdb6fbc7dfc6cbd1ef69b9fd57c25d916e7523
Cameron Nieve <camnieve@gmail.com>
2022-01-21 14:06:51 +0800

    Add an input box

--- src/index.js
+++ src/index.js
@@ -26,14 +26,18 @@ function TasksList() {
   ];
 
   return(
-    <ul>
-      {/* display object collection taskItemsList using array map */}
-      {/* callback function (map) that's why it needs "return" */}
-      {/* to solve "Each child in a list should have a unique "key" prop." warning, include index */}
-      {taskItemsList.map((task, index) => {
-        return <Task key={index} taskName={task} />
-      })}
-    </ul>
+    //adding input
+    <div>
+      <input className="task-input" />
+      <ul>
+        {/* display object collection taskItemsList using array map */}
+        {/* callback function (map) that's why it needs "return" */}
+        {/* to solve "Each child in a list should have a unique "key" prop." warning, include index */}
+        {taskItemsList.map((task, index) => {
+          return <Task key={index} taskName={task} />
+        })}
+      </ul>
+    </div>
   );
 }
 
