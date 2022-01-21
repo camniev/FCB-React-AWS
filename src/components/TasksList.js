7613748deac6f0b8ad492ce2e11670c0c945b365
Cameron Nieve <camnieve@gmail.com>
2022-01-21 15:00:02 +0800

    Create the inputTaskHandler and useState hook

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import Task from "./Task";
 
 function TasksList() {
 
     // data - object collection of tasks
     const taskItemsList = [
-      "Follow Edukasyon.ph on Facebook",
-      "Follow AWS Siklab Pilipinas on Facebook",
-      "Follow Zuitt Coding Bootcamp on Facebook"
+        "Follow Edukasyon.ph on Facebook",
+        "Follow AWS Siklab Pilipinas on Facebook",
+        "Follow Zuitt Coding Bootcamp on Facebook"
     ];
+
+    // usestate is a react feature to store and update temporary information
+    // destructuring in const (review) 
+
+    const[taskValue, setTaskValue] =  useState("Just another task");
+    console.log("task value: " + taskValue);
+
+    //create event to update data in setTaskValue
+    const inputChangeHandler = (e) => {
+        setTaskValue(e.target.value)
+    };
   
     return(
       //adding input
       <div>
-        <input className="task-input" />
+
+        {/* create event on change (review) */}
+        {/* <input
+            className="task-input"
+            placeholder="Create a new task"
+            onChange={(e) => setTaskValue(e.target.value)}
+        /> */}
+
+        <input
+            className="task-input"
+            placeholder="Create a new task"
+            onChange={inputChangeHandler}
+        />
+
         <ul>
           {/* display object collection taskItemsList using array map */}
           {/* callback function (map) that's why it needs "return" */}
